1d4b2bdb6e11e077208a7248dac39cddcb04ca95
Your Name <you@example.com>
2023-08-09 02:39:24 +0800

    Update user select position

--- Minesweeper.php
+++ Minesweeper.php
@@ -16,69 +16,26 @@
             $mode = EASY;
     }
 
-    //Decide where mines position
-    /*$mines = 0;
-    $minesCoordinate = array();
-
-    while($mines < $mode['mines']) {
-        $minesLocationX = mt_rand(0, $mode['range'][0] - 1);
-        $minesLocationY = mt_rand(0, $mode['range'][1] - 1);
-
-        if(empty($minesLocation[$minesLocationX][$minesLocationY])) {
-            $minesLocation[$minesLocationX][$minesLocationY] = 1;
-            echo $minesLocation[$minesLocationX][$minesLocationY];
-        }
-
-        $mines++;
-    };
-    while($mines < $mode['mines']) {
-        $minesCoordinateX = mt_rand(0, $mode['range'][0] - 1);
-        $minesCoordinateY = mt_rand(0, $mode['range'][1] - 1);
-        $minesTemporaryCoordinate = "$minesCoordinateX,$minesCoordinateY";
-
-        if(!in_array($minesTemporaryCoordinate, $minesCoordinate)) {
-            $minesCoordinate[] = $minesTemporaryCoordinate;
-            $mines++;
-        }
-    }*/
     $minesweeperService = new MinesweeperService();
+
     $mines = $minesweeperService->mines;
     $minesCoordinate = $minesweeperService->minesCoordinate;
+
     $minesPosition = $minesweeperService->decideMinesCoordinate($mines, $minesCoordinate, $mode);
 
     print_r($minesPosition);
 
     //User select position
-    /*$selectCoordinateX = readline("Enter Coordinate X:")-1;
-    while(($selectCoordinateX > $mode['range'][0] - 1) || $selectCoordinateX < 0) {
-        echo "Invalid Coordinate, value must between 1~".$mode['range'][0]."\n";
-        $selectCoordinateX = readline("Enter Coordinate X:")-1;
-    }
-
+    $selectCoordinateX = readline("Enter Coordinate X:")-1;
     $selectCoordinateY = readline("Enter Coordinate Y:")-1;
-    while(($selectCoordinateY > $mode['range'][1] - 1) || $selectCoordinateY < 0) {
-        echo "Invalid Coordinate, value must between 1~".$mode['range'][1]."\n";
-        $selectCoordinateY = readline("Enter Coordinate Y:")-1;
-    }
 
-    $selectCoordinate = "$selectCoordinateX,$selectCoordinateY";
+    $selectPosition = $minesweeperService->userSelectPosition($selectCoordinateX, $selectCoordinateY, $mode);
 
-    //
-    if(in_array($selectCoordinate, $minesCoordinate)) {
-        echo "You Died"."\n";
-        return;
-    }
 
-    $numberOfMines = 0;
-    foreach($minesCoordinate as $coordinates) {
-        $coordinatesX = explode(",", $coordinates)[0];
-        $coordinatesY = explode(",", $coordinates)[1];
-        $squareOfDistance = pow($selectCoordinateX-$coordinatesX, 2) + pow($selectCoordinateY-$coordinatesY, 2);
-        if($squareOfDistance==2 || $squareOfDistance==1) {
-            $numberOfMines++;
-        }
-    }
 
-    echo $numberOfMines."\n";*/
+
+
+
+    //echo $numberOfMines."\n";
 
     //print_r($mode);
--- MinesweeperService.php
+++ MinesweeperService.php
@@ -30,4 +30,42 @@
             return $minesCoordinate;
         }
 
+        //User select position
+        public function userSelectPosition($selectCoordinateX, $selectCoordinateY, $mode) {
+            while(($selectCoordinateX > $mode['range'][0] - 1) || $selectCoordinateX < 0) {
+                echo "Invalid Coordinate, X value must between 1~".$mode['range'][0]."\n";
+                $selectCoordinateX = readline("Enter Coordinate X:")-1;
+            }
+
+            while(($selectCoordinateY > $mode['range'][1] - 1) || $selectCoordinateY < 0) {
+                echo "Invalid Coordinate, Y value must between 1~".$mode['range'][1]."\n";
+                $selectCoordinateY = readline("Enter Coordinate Y:")-1;
+            }
+
+            $selectCoordinate = "$selectCoordinateX,$selectCoordinateY";
+
+            return $selectCoordinate;
+        }
+
+        //
+        public function checkNumberOfMines ($selectPosition, $minesPosition) {
+            /*if(in_array($selectPosition, $minesPosition)) {
+                echo "You Died"."\n";
+                return;
+            }*/
+            while(!in_array($selectPosition, $minesPosition)) {
+
+                $numberOfMines = 0;
+                foreach ($minesPosition as $coordinates) {
+                    $coordinatesX = explode(",", $coordinates)[0];
+                    $coordinatesY = explode(",", $coordinates)[1];
+                    $squareOfDistance = pow($selectCoordinateX - $coordinatesX, 2) + pow($selectCoordinateY - $coordinatesY, 2);
+                    if ($squareOfDistance == 2 || $squareOfDistance == 1) {
+                        $numberOfMines++;
+                    }
+                }
+            }
+
+            echo "You Died!!";
+        }
     }
